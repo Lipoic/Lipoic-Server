105fe2c5ebc9d0fd9fd8e906862c9677a335460b
Mg138 <muguang138@gmail.com>
2022-05-03 21:44:55 +0800

    init secrets and mongodb

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,18 @@
 #[macro_use]
 extern crate rocket;
 
+use mongodb::{bson::doc, options::ClientOptions, Client};
 use rocket::Request;
 use rocket_contrib::json::Json;
+
+mod secrets;
 
 mod data;
 mod util;
 
-use data::response::Response;
+use crate::data::response::Response;
+
+use crate::secrets::mongodb::URL;
 
 #[catch(404)]
 fn not_found(req: &Request) -> Json<Response> {
@@ -26,9 +31,32 @@ fn index() -> Json<Response> {
     Json(Response::ok(&Some("hello world!")))
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> mongodb::error::Result<()> {
+    let mut client_options = ClientOptions::parse(format!("mongodb+srv://{}", URL)).await?;
+
+    // Manually set an option
+    client_options.app_name = Some("Rust Demo".to_string());
+
+    // Get a handle to the cluster
+    let client = Client::with_options(client_options)?;
+
+    // Ping the server to see if you can connect to the cluster
+    client
+        .database("admin")
+        .run_command(doc! {"ping": 1}, None)
+        .await?;
+    println!("Connected successfully.");
+
+    // List the names of the databases in that cluster
+    for db_name in client.list_database_names(None, None).await? {
+        println!("{}", db_name);
+    }
+
     rocket::ignite()
         .register(catchers![not_found])
         .mount("/", routes![index, teapot])
         .launch();
+
+    Ok(())
 }
