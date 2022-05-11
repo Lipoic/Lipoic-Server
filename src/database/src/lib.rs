556c72764f314a4e4559f290da9fa7517399e1a9
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2022-05-11 21:46:34 +0800

    feat(database): dev mode and deploy mode

--- src/database/src/lib.rs
+++ src/database/src/lib.rs
@@ -10,9 +10,15 @@ pub struct DB {
 /// init mongodb
 pub async fn init() -> mongodb::error::Result<DB> {
     // Load environment variables
-    dotenv::dotenv().expect("Failed to load .env file");
-
-    let mongodb_url = env::var("MONGODB_URL").unwrap();
+    let mongodb_url = if let Ok(url) = env::var("MONGODB_URL") {
+        // deploy mode
+        url
+    } else {
+        // dev mode
+        // load .env file
+        dotenv::dotenv().expect("Failed to load .env file");
+        env::var("MONGODB_URL").unwrap()
+    };
     let mut client_options = ClientOptions::parse(mongodb_url).await?;
 
     // Manually set an option
