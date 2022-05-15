2ee1adb68379e61aa30107059a399d1b41aff158
SiongSng <rrt467778@gmail.com>
2022-05-15 14:08:27 +0800

    feat: delete teapot route

--- src/router/src/resource.rs
+++ src/router/src/resource.rs
@@ -7,11 +7,6 @@ fn hello_world() -> Json<Response> {
     Response::default().ok(&Some("hello world!")).into()
 }
 
-#[get("/teapot")]
-fn teapot() -> Json<Response> {
-    Response::default().teapot(&Some("I'm a teapot!")).into()
-}
-
 pub fn stage() -> AdHoc {
     AdHoc::on_ignite("catch stage", |rocket| async {
         rocket.mount("/", routes![hello_world, teapot])
