4ad5de1618d7c1ef8fc56da1add84e856d91dd57
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2022-05-15 21:56:09 +0800

    util: google OAuth2 add unit test

---
+++ src/util/tests/oauth_test.rs
@@ -0,0 +1,16 @@
+use util::oauth::GoogleOAuth;
+
+const CLIENT_ID: &str = "";
+const CLIENT_SECRET: &str = "";
+
+#[test]
+fn google_oauth2_get_auth_url_test() {
+    let oauth = GoogleOAuth::new(
+        CLIENT_ID.to_string(),
+        CLIENT_SECRET.to_string(),
+        "http://127.0.0.1:8000".to_string(),
+        "/"
+    );
+
+    println!("{}", oauth.get_auth_url())
+}
