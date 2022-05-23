c7665470ec2e7319359d7c706c8820801615ad2c
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2022-05-23 11:14:31 +0800

    fix: test mod

--- src/main/src/main.rs
+++ src/main/src/main.rs
@@ -1,6 +1,6 @@
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
-    let _ = router::rocket().await.launch().await?;
+    let _ = router::rocket(false).await.launch().await?;
 
     Ok(())
 }
