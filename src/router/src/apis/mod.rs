f8308a5e100c59d92d16ed854027446dede54f63
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2022-05-28 13:05:40 +0800

    fix(router): apis add verify email stage

--- src/router/src/apis/mod.rs
+++ src/router/src/apis/mod.rs
@@ -6,5 +6,6 @@ use rocket::fairing::AdHoc;
 pub fn stage() -> AdHoc {
     AdHoc::on_ignite("load api stage", |rocket| async {
         rocket.attach(authentication::stage())
+            .attach(verify_email::stage())
     })
 }
