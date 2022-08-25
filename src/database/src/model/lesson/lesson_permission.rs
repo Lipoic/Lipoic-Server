ab0e373adec9221da5eb2ef2ab35f3990f8de4a9
SiongSng <rrt467778@gmail.com>
2022-08-25 15:14:42 +0800

    style: remove unused import

--- src/database/src/model/lesson/lesson_permission.rs
+++ src/database/src/model/lesson/lesson_permission.rs
@@ -1,7 +1,6 @@
 use mongodb::bson::oid::ObjectId;
 use serde::Deserialize;
 use serde::Serialize;
-use rocket::form::FromForm;
 use rocket::form::FromFormField;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
